8901114e378dc43dfad6a5151723e211d1e4763a
Kam1runetz <notsoserious2017@gmail.com>
2021-03-10 22:14:31 +0300

    int_array ready

---
+++ src/int_array/include/int_array.h
@@ -0,0 +1,31 @@
+// Copyright 2021 Kam1runetzLabs <notsoserious2017@gmail.com>
+
+#ifndef INCLUDE_INT_ARRAY_H_
+#define INCLUDE_INT_ARRAY_H_
+
+#include <stddef.h>
+
+typedef struct {
+    int *begin;
+    int *end;
+} int_array_t;
+
+typedef int *iterator;
+typedef const int *const_iterator;
+
+///\brief initialize array with transmitted size
+///\param size - size of the array to create
+///\return pointer to the created array or NULL if allocation error happened
+int_array_t *init_array(size_t size);
+
+///\brief initialize array by copy of existing array
+///\param begin - start of copying memory
+///\param end - end of copying memory
+///\return pointer to the created array or NULL if error
+int_array_t *init_array_from_mem(const_iterator begin, const_iterator end);
+
+///\brief frees the memory from the array
+///\param array - pointer to array to delete, may be NULL
+void delete_array(int_array_t *array);
+
+#endif  // INCLUDE_INT_ARRAY_H_
---
+++ src/int_array/sources/int_array.c
@@ -0,0 +1,40 @@
+// Copyright 2021 Kam1runetzLabs <notsoserious2017@gmail.com>
+
+#include <int_array.h>
+#include <memory.h>
+#include <stdlib.h>
+
+int_array_t *init_array(size_t size) {
+    if (!size) return NULL;
+    int_array_t *array = (int_array_t *) malloc(sizeof(int_array_t));
+    if (!array) return NULL;
+    array->begin = (int *) malloc(sizeof(int) * size);
+    if (!array->begin) {
+        free(array);
+        return NULL;
+    }
+    array->end = array->begin + size;
+    return array;
+}
+
+void delete_array(int_array_t *array) {
+    if (!array) return;
+    if (array->begin) free(array->begin);
+    array->begin = array->end = NULL;
+    free(array);
+}
+
+int_array_t *init_array_from_mem(const_iterator begin, const_iterator end) {
+    if (!begin) return NULL;
+    if (begin >= end) return NULL;
+    int_array_t *array = (int_array_t *) malloc(sizeof(int_array_t));
+    if (!array) return NULL;
+    array->begin = (int *) malloc(sizeof(int) * (end - begin));
+    if (!array->begin) {
+        free(array);
+        return NULL;
+    }
+    memcpy(array->begin, begin, sizeof(int) * (end - begin));
+    array->end = array->begin + (end - begin);
+    return array;
+}
