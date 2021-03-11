0b6d6fb203cc573fbf52114c9ed914fb7f934f59
Kam1runetz <notsoserious2017@gmail.com>
2021-03-11 06:50:49 +0300

    added html coverage report

--- scripts/coverage.sh
+++ scripts/coverage.sh
@@ -8,9 +8,8 @@ CMAKE_CONFIG_OPTS="-DCMAKE_BUILD_TYPE=Debug"
 CMAKE_TOOLCHAIN_OPTS="-DCMAKE_TOOLCHAIN_FILE='$(pwd)/tools/polly/gcc-pic.cmake'"
 CMAKE_OPTS="$CMAKE_CONFIG_OPTS $CMAKE_TOOLCHAIN_OPTS"
 
-# todo fix cover
-# todo html cover report
 cmake -H. -B_builds -DBUILD_COVERAGE=ON "$CMAKE_OPTS"
 cmake --build _builds
 cmake --build _builds --target test
-gcovr -r .
+mkdir coverage-report
+gcovr -r . --html --html-details -o coverage-report/coverage-report.html
