375002096ab89fc2de7d3cd77b260c379bd5b33d
Kam1runetz <notsoserious2017@gmail.com>
2021-03-11 07:20:52 +0300

    trying get reports from travis ci

--- scripts/static_analyze.sh
+++ scripts/static_analyze.sh
@@ -3,8 +3,8 @@
 CXX_CPPCHECK_OPTIONS="--language=c --std=c11 --verbose "
 C_CPPCHECK_OPTIONS="--language=c++ --std=c++11 --verbose "
 
-cppcheck $C_CPPCHECK_OPTIONS --xml --xml-version=2 ./src 2>coverage-report-src.xml
-cppcheck-htmlreport --file=coverage-report-src.xml --report-dir=covergae-report-src
+cppcheck $C_CPPCHECK_OPTIONS --xml --xml-version=2 ./src 2>stat-analyze-report-src.xml
+cppcheck-htmlreport --file=stat-analyze-report-src.xml --report-dir=covergae-report-src
 
-cppcheck $CXX_CPPCHECK_OPTIONS --xml --xml-version=2 ./tests 2>coverage-report-tests.xml
-cppcheck-htmlreport --file=coverage-report-tests.xml --report-dir=coverage-report-tests
+cppcheck $CXX_CPPCHECK_OPTIONS --xml --xml-version=2 ./tests 2>stat-analyze-report-tests.xml
+cppcheck-htmlreport --file=stat-analyze-report-tests.xml --report-dir=stat-analyze-report-tests
