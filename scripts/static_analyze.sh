40fadf9bbc9010f4098241671ca9e635baca40dd
Kam1runetz <notsoserious2017@gmail.com>
2021-03-11 06:46:42 +0300

    static_analyze.sh refactored

--- scripts/static_analyze.sh
+++ scripts/static_analyze.sh
@@ -3,8 +3,8 @@
 CXX_CPPCHECK_OPTIONS="--language=c --std=c11 --verbose "
 C_CPPCHECK_OPTIONS="--language=c++ --std=c++11 --verbose "
 
-cppcheck $C_CPPCHECK_OPTIONS --xml --xml-version=2 ./src 2>report-src.xml
-cppcheck-htmlreport --file=report-src.xml --report-dir=report-src
+cppcheck $C_CPPCHECK_OPTIONS --xml --xml-version=2 ./src 2>coverage-report-src.xml
+cppcheck-htmlreport --file=coverage-report-src.xml --report-dir=covergae-report-src
 
-cppcheck $CXX_CPPCHECK_OPTIONS --xml --xml-version=2 ./tests 2>report-tests.xml
-cppcheck-htmlreport --file=report-tests.xml --report-dir=report-tests
+cppcheck $CXX_CPPCHECK_OPTIONS --xml --xml-version=2 ./tests 2>coverage-report-tests.xml
+cppcheck-htmlreport --file=coverage-report-tests.xml --report-dir=coverage-report-tests
